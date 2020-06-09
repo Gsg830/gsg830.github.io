6218fc337d3a186c47a06810162b77a354957aba
Gsg830 <66660917+Gsg830@users.noreply.github.com>
2020-06-09 00:56:04 -0400

    Create triangle_solver.py

---
+++ triangle_solver.py
@@ -0,0 +1,26 @@
+"The hypotenuse of a right triangle is 65cm. The sum of the other two sides is 79cm. Find the lengths of the other two sides of the rectangle. (16cm,63cm)"
+
+a = int(input("enter a + b value: ")) #max value imput
+b = 0 #min value
+r = int(input("enter 'c' value: ")) # enter 'c' value
+c = r*r
+d = 1
+print()
+print()
+
+for x in range(0,a):
+  if a < 0 :
+    print("Not possible")
+    break
+  elif a > 0:
+    print("group " + str(d) + ": ")
+    print("a = " + str(a))
+    print("b = " + str(b) + "\n")
+  if a**2+b**2 == c:
+    print("Answer is group " + str(d))
+    break
+  b +=1
+  a -=1
+  d +=1
+  
+  
